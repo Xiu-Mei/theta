a3319604cb3d01d6c3438e866a17434b90fa9da2
Sl <pogody@gmail.com>
2018-09-04 18:39:00 +0300

    fix location view

--- location/views.py
+++ location/views.py
@@ -58,10 +58,11 @@ class FloorTemplateView(OfficeAdminValidationMixin, TemplateView):
 
     def __init__(self):
         super(FloorTemplateView, self).__init__()
-        self.params = dict()
-        self.context = dict()
+        self.get_params = dict()
         self.post_params = dict()
-        self.validation_params = dict()
+        self.context = dict()
+        self.params = dict()
+        self.action = ''
 
     def get(self, request, **kwargs):
         if self.is_office_admin() is False:
@@ -87,82 +88,99 @@ class FloorTemplateView(OfficeAdminValidationMixin, TemplateView):
 
         self.params['building_id'] = kwargs['building_id']
         self.params['floor_id'] = kwargs['floor_id']
+
         if self.get_floor() is False:
             return redirect('location')
 
-        self.post_params['place_id'] = self.request.POST.get('id')
-        self.validation_params['place_id'] = {
+        if not request.is_ajax():
+            return redirect('location')
+
+        self.get_post_params() and self.get_post_context()
+
+        if 'error' not in self.context.keys():
+            return JsonResponse(self.context)
+        else:
+            return JsonResponse({'error': self.context['error']})
+
+    def get_post_params(self):
+        actions = dict()
+        validation_params = dict()
+
+        actions['save'] = (('place_id', 'id'), 'left', 'top', 'text', )
+        actions['delete'] = (('place_id', 'id'), )
+
+        validation_params['place_id'] = {
             'symbol_set': 'int',
         }
-        self.post_params['left'] = self.request.POST.get('left')
-        if self.post_params['left'].endswith('px'):
-            self.post_params['left'] = self.post_params['left'][:-2]
-        self.validation_params['left'] = {
-            'symbol_set': 'int',
+        validation_params['left'] = {
+            'symbol_set': 'd',
             'required': True,
         }
-        self.post_params['top'] = self.request.POST.get('top')
-        if self.post_params['top'].endswith('px'):
-            self.post_params['top'] = self.post_params['top'][:-2]
-        self.validation_params['top'] = {
-            'symbol_set': 'int',
+        validation_params['top'] = {
+            'symbol_set': 'd',
             'required': True,
         }
-        self.post_params['text'] = self.request.POST.get('text')
-        self.validation_params['text'] = {
+        validation_params['text'] = {
             'symbol_set': 'w',
             'allowed': '_-',
             'return_sentence': True,
         }
-        self.post_params['action'] = self.request.POST.get('action')
-        self.validation_params['action'] = {
-            'symbol_set': 'en_sm',
-            'allowed_values': ('save', 'delete', ),
+
+        self.action = self.request.POST.get('action')
+        validation_params['action'] = {
+            'symbol_set': 'en',
+            'allowed_values': tuple(key for key in actions.keys()),
             'required': True,
         }
-        if request.is_ajax():
-            if validation(self.post_params, self.validation_params) is False:
-                return JsonResponse({'error': self.post_params['error']})
+        if not validation({'action': self.action}, validation_params):
+            return False
 
-            self.change_place()
-            print(self.context['id'])
-            return JsonResponse(self.context)
-        else:
-            return redirect('location')
-
-    def change_place(self):
-        if self.post_params['action'] == 'save':
-            if self.post_params['place_id']:
-                place = Place.objects.filter(floor_id=self.params['floor'], id=self.post_params['place_id']).\
-                    update(
-                    left=self.post_params['left'],
-                    top=self.post_params['top'],
-                    name=self.post_params['text']
-                )
-                self.context['id'] = self.post_params['place_id'] if place else 0
+        for item in actions[self.action]:
+            if type(item) is not tuple:
+                self.post_params[item] = self.request.POST.get(item)
             else:
-                place = Place.objects.create(
-                    floor_id=self.params['floor_id'],
-                    left=self.post_params['left'],
-                    top=self.post_params['top'],
-                    name=self.post_params['text']
-                )
-                self.context['id'] = place.id if place else 0
+                self.post_params[item[0]] = self.request.POST.get(item[1])
+        return validation(self.post_params, validation_params)
 
-        elif self.post_params['action'] == 'delete':
-            Place.objects.filter(floor_id=self.params['floor'], id=self.post_params['place_id']).delete()
-            self.context['id'] = 0
+    def save_place(self):
+        if self.post_params['place_id']:
+            place = Place.objects.filter(floor_id=self.params['floor'], id=self.post_params['place_id']). \
+                update(
+                left=self.post_params['left'],
+                top=self.post_params['top'],
+                name=self.post_params['text']
+            )
+            self.context['id'] = self.post_params['place_id'] if place else 0
+        else:
+            place = Place.objects.create(
+                floor_id=self.params['floor_id'],
+                left=self.post_params['left'],
+                top=self.post_params['top'],
+                name=self.post_params['text']
+            )
+            self.context['id'] = place.id if place else 0
+
+    def delete_place(self):
+        Place.objects.filter(floor_id=self.params['floor'], id=self.post_params['place_id']).delete()
+        self.context['id'] = 0
+
+    def get_post_context(self):
+        if self.action == 'save':
+            self.save_place()
+        elif self.action == 'delete':
+            self.delete_place()
 
     def get_floor(self):
         try:
-            Building.objects.get(
+            building = Building.objects.get(
                 office=self.office,
                 id=self.params['building_id'],
             )
             self.params['floor'] = Floor.objects.get(
-                building=self.params['building_id'],
+                building=building,
                 id=self.params['floor_id'],
             )
         except (Building.DoesNotExist, Floor.DoesNotExist):
             self.params['error'] = 'Building or Floor doesn\'t  exitst'
             return False
+
